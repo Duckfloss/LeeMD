270780395e64900e7643cfb37f124789c53c5223
BenCJones <ben@bencjones.com>
2015-12-15 16:40:12 -0500

    Move command line control to leemd.rb, improve argv processing

--- leemd.rb
+++ leemd.rb
@@ -1,9 +1,8 @@
+#!/usr/bin/env ruby
+
 #TODO: sublists in list style
 #TODO: skip empty lines in lists
 #TODO: add vendor name based on VCS field
-
-# Add lib to load path
-$LOAD_PATH << "./leemd"
 
 # Require Gems
 require 'csv'
@@ -11,93 +10,74 @@ require 'htmlentities'
 require 'optparse'
 require 'ostruct'
 
+leemdconvert = 'c:/Documents and Settings/pos/desktop/website/toolbox/leemd/lib/LeeMDConvert.rb'
+
 # Require Classes
-require 'LeeMDConvert'
+require leemdconvert
 
-class Parser
-
-	FORMATS = ["all","t","med","lg","sw"]
-
-	def self.parse(args)
-		options = OpenStruct.new
-		options.format = ["all"]
-		options.eci = false
-		options.source = nil
-		options.dest = nil
-		options.verbose = false
-
-		opt_parser = OptionParser.new do |opt|
-			opt.banner = "Usage: limg.rb [options]"
-			opt.separator ""
-			opt.separator "Options:"
-
-			opt.on("--source SOURCE", "Sets source file or directory", "  default is Downloads/WebAssets") do |source|
-				# Validate source
-				if !File.directory?(source)
-					if File.exist?(source)
-						options.source = { "file"=>source }
-					end
-				else
-					options.source = { "dir"=>source }
-				end
-
-				if options.source.nil?
-					puts "error" #error
-				end
-			end
-
-			opt.separator ""
-
-			opt.on("--dest DEST", "Sets destination directory", "  defaults are R:/RETAIL/IMAGES/4Web", "  and R:/RETAIL/RPRO/Images/Inven") do |dest|
-				if !Dir.exist?(dest)
-					puts "error" #error
-				else
-					options.dest = dest
-				end
-			end
-
-			opt.separator ""
-
-			opt.on("-e", "--eci", "Parses pic(s) to ECI's directory", "  as well as to default or selected destination") do
-				options.eci = true
-			end
-
-			opt.separator ""
-
-			opt.on("-fFORMAT", "--format FORMAT", Array, "Select output formats", "  accepts comma-separated string", "  output sizes are t,sw,med,lg", "  default is \"all\"") do |formats|
-				formats.each do |format|
-					if FORMATS.index(format.downcase).nil?
-						puts "error" #error
-						exit
-					end
-					options.format = formats
-				end
-			end
-
-			opt.separator ""
-
-			opt.on("-v", "--verbose", "Run chattily (or not)", "  default runs not verbosely") do |v|
-				options.verbose = true
-			end
-
-			opt.separator ""
-
-			opt.on_tail("-h","--help","Prints this help") do
-				puts opt
-				exit
-			end
-		end
-
-		opt_parser.parse!(args)
-		options
+def valid_file?(file)
+	if file.nil?
+		puts "Please provide a file to format"
+		exit 0
 	end
+	if !file =~ /\.csv$/
+		puts "Requires a CSV-formated spreadsheet"
+		exit 0
+	end
+	if !File.exists?(file)
+		puts "#{file} doesn't seem to exist. Please check\nyour file path and try again."
+		exit 0
+	end
+	true
+end
+
+def parse_args
+
+	options = OpenStruct.new
+	options.source = nil
+	options.verbose = false
+
+	if ARGV.include?("-h")
+		print_help
+	end
+
+	if ARGV.include?("-v")
+		ARGV.delete("-v")
+		options.verbose = true
+	end
+
+	if valid_file?(ARGV[0])
+		options.source = File.absolute_path(ARGV[0])
+	end
+
+	options
+
+end
+
+def print_help
+	puts "Command takes a CSV-formated file, parses the \"desc\"\n"
+	puts "field and, returns a new file formated for posting on\n"
+	puts "the web. See the online manual for LeeMD shortcode rules.\n"
+	puts "\n"
+	puts "Usage: leemd.rb FILE.csv [options]\n"
+	puts "\n"
+	puts "Options:\n"
+	puts "\t-v\t\t\tRuns verbosely\n"
+	puts "\t-h\t\t\tPrints this help\n"
+	exit 0
 end
 
 if __FILE__ == $0
 
-options = Parser.parse(ARGV)
-puts options.to_h
-#Image_Chopper.new(options)
+options = parse_args
+
+path = options.source.slice(0,options.source.index(/\/[A-Za-z0-9\-\_]+\.csv$/)+1)
+file = options.source.slice(/[A-Za-z0-9\-\_]+\.csv$/)
+
+$csv_source = options.source
+$csv_target = "#{path}FILTERED#{file}"
+
+doit($csv_source, $csv_target)
 
 end
 
@@ -110,4 +90,5 @@ file = $csv_source.slice(/[A-Za-z0-9\-\_]+\.csv$/)
 $csv_target = "#{path}FILTERED#{file}"
 
 doit($csv_source, $csv_target)
+
 =end
